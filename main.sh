2f7372c4cdae42aaba37dbca0c1dc32ce22da83f
iamshreeram <shreeram.v@live.com>
2018-08-05 21:37:24 -0600

    Updating main.sh to read app.conf file

--- main.sh
+++ main.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #set -x
 STATFILE='StatusOutput'
+OUT='index.html'
+CONFIG='app.conf'
+INST_URLS=instanceurl_file
+INST_NAMES=instance_names
+
 HEADER='<!DOCTYPE html><html lang="en"><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta></head><style>body{background-color:black}.pass{fill:green}.fail{fill:red}.warn{fill:orange}.status{fill:gray}svg{font-size:14px;fill:#fff;background-color:black;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"}text.month-90deg{transform:rotate(90deg)}</style><body><br /><br /><br /><div><div><svg width="1900" height="1050" >'
 FOOTER='</svg></div></div></div></body></html>'
 GTRANSFORM='<g transform="translate(0, 20)">'
@@ -8,56 +13,69 @@ CLOSEG='</g>'
 TILE='<rect class=status width=20 height=20 x=xdata y=ydata v=version />'
 CLOCK='<text x=xtimloc y=ytimloc>hour:00</text>'
 COMP='<text dx=xaxis dy=yaxis>comp</text>'
-OUT='index.html'
 ver_re='^[0-9]+([.][0-9]+)?$'
-url_file=$1
+
+read_config() {
+	CONFIG=`cat $CONFIG`;
+	if [[ `echo $CONFIG` == *,* ]];
+	then
+			COMPHEADER=`echo $CONFIG | sed 's/,[A-Za-z0-9.:-]*//g' | sed 's/\/[A-Za-z0-9-]*//g'`;
+			COMPURL=`echo $CONFIG | sed 's/[A-Za-z0-9_-/]*,//g'`;
+	elif [[ `echo $CONFIG` == */* ]];
+	then
+			COMPHEADER=`echo $CONFIG | sed 's/[A-Za-z0-9.:-]*\///g'`;
+			COMPURL=`echo $CONFIG`;
+	elif ! ([[ `echo $CONFIG` == *,* ]] || [[ `echo $CONFIG` == */* ]]);
+	then
+		COMPHEADER=`cat $INST_NAMES`
+		COMPURL=`cat $INST_URLS`
+	else
+		echo "<h1>Can't monitor the application as the config file is missing or corrupted</h1>">>$OUT
+		exit 1
+	fi;
+}
 
 
 statfile_checker(){
 if [[ ! -z ${STATFILE} ]];
-then
-        rm $STATFILE
-fi
+	then
+		rm $STATFILE
+	fi
 }
 
 remove_vhost()
 {
-if [[ ! -z vhost_unknown* ]];
-then
-        rm vhost_unknown*
-fi
+	if [[ ! -z vhost_unknown* ]];
+	then
+		rm vhost_unknown*
+	fi
 }
 
 indexfile_checker(){
-  if [[ ! -z ${OUT} ]];
-  then
-      rm $OUT
-  fi
+	if [[ ! -z ${OUT} ]];
+	then
+	  rm $OUT
+	fi
 }
 
 index_start(){
- echo $HEADER>>$OUT
- echo $GTRANSFORM>>$OUT
+	echo $HEADER>>$OUT
+	echo $GTRANSFORM>>$OUT
 }
 
-file_read()
+get_compnt_list()
 {
-	apps=`cat url_file | awk -F'/' '{print $2}'` 
+	# apps=`cat $COMPHEADERFILE`
+	apps=`echo $COMPHEADER`
 }
 
-file_checker(){
-if [[ ! -z ENVSTATUS ]];
-then
-        rm ENVSTATUS
-fi
-}
 
 add_tile(){
-  xdata=$1
-  ydata=$2 
-  status=$3
-  version=$4
-  echo $TILE | sed "s/xdata/$xdata/g" | sed "s/ydata/$ydata/g" | sed "s/status/$status/g" |  sed "s/version/$version/g" >>$OUT
+	xdata=$1
+	ydata=$2
+	status=$3
+	version=$4
+	echo $TILE | sed "s/xdata/$xdata/g" | sed "s/ydata/$ydata/g" | sed "s/status/$status/g" |  sed "s/version/$version/g" >>$OUT
 }
 
 index_addcomp(){
@@ -65,7 +83,7 @@ index_addcomp(){
   yaxis=20
   xdata=350
   ydata=5
-  for app in `cat url_file | awk -F'/' '{print $2}'`
+  for app in $apps
     do
       echo $COMP | sed "s/comp/$app/g" | sed "s/xaxis/$xaxis/g" | sed "s/yaxis/$yaxis/g">>$OUT
       yaxis=$((yaxis+30))
@@ -82,44 +100,41 @@ index_addcomp(){
 
 index_addafterblack(){
   for xdata in {1800..1850..30}
-    do 
+    do
       echo "<rect width=20 height=20 fill="#000000" x=$xdata y=0 />">>$OUT
     done
 }
 
-
 status_puller(){
-for e in url_file 
-do
 ydata=5
 xdata=$1
-        file=$e
-        while IFS= read -r line
-        do
-                val=$(wget  -S "http://$line/version.html" 2>&1 | grep "HTTP/" | awk '{print $2}')
-                if [[ $val == 200 ]]; then
-                        version_no=$(grep Version version.html|awk  '{print $3}'|awk -F '<' '{print $2}'|awk -F '>' '{print $2}')
-						if ! [[ $yournumber =~ $ver_re ]] ; then
-							add_tile $xdata $ydata "pass" "NOTFOUND"
-						else
-							add_tile $xdata $ydata "pass" $version_no
-						fi
-						rm -rf version.html*
-                else
-						add_tile $xdata $ydata "fail" "ERROR"
-						rm -rf version.html*
-                fi
-		ydata=$((ydata+30))
-        done <"$file"
+for line in $COMPURL
+	do
+	val=$(wget  -S "http://$line/version.html" 2>&1 | grep "HTTP/" | awk '{print $2}')
+	if [[ $val == 200 ]]; then
+		version_no=$(grep Version version.html|awk  '{print $3}'|awk -F '<' '{print $2}'|awk -F '>' '{print $2}')
+		if ! [[ $yournumber =~ $ver_re ]] ; then
+			add_tile $xdata $ydata "pass" "NOTFOUND"
+		else
+			add_tile $xdata $ydata "pass" $version_no
+		fi
+			rm -rf version.html*
+		else
+			add_tile $xdata $ydata "fail" "ERROR"
+			rm -rf version.html*
+	fi
+	ydata=$((ydata+30))
 done
 }
+
+
 
 executor(){
   for xdata in {350..1760..30}
     do
-      status_puller $xdata
-	  remove_vhost
-      sleep 30m
+        status_puller $xdata
+        remove_vhost
+        sleep 10s
   done
 }
 
@@ -132,7 +147,8 @@ main(){
 	statfile_checker
 	indexfile_checker
 	index_start
-	file_read
+	read_config
+	get_compnt_list
 	index_addcomp
 	executor
 	index_end
